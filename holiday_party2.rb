8a2d489387499bb64aff33b4dec15af5cd235839
EleanorCallottWhitney <eleanor.whitney@gmail.com>
2013-12-24 01:27:29 -0500

    fixed with a lot of help from my friends

--- holiday_party2.rb
+++ holiday_party2.rb
@@ -1,20 +1,30 @@
+guests = %w{George Liz Kristen Matt Martha}
+friends = %w{Liz Kristen Matt}
 
-
-$partyguests = [ :David, :Liz, :Courtney, :Peter, :Maria]
-$like_to_talk_to = [ :Liz, :Courtney, :Peter]
-
-	def conversations	
-		$partyguests.each do |talk|
-			if $like_to_talk_to.include? talk
-				puts "Hi, #{talk}, happy holidays! What are you doing for the break?"
-				partyguest_response = gets.to_s
-				puts "Oh, #{partyguest_response}, great! I'm" + [" staying at home with my cat.", " making seven gallons of soup.", " figuring out how Rails works."].sample
-				puts "Well, let's hang out soon! Happy New Year!"
-			else puts "Oh, hi #{talk}, is that another bottle of wine and some Brie? Excuse me, won't you..."	
-			end	 	
-		end
-	puts "Bye! Have great holidays! I have to go" + [" work my co-op shift.", " repaint my living room.", " do more Ruby tutorials." ].sample	
-end	
-conversations()
+def conversations(guests, friends)  
+  guests.each do |guest|
+    if friends.include?(guest)
+      puts "Hi, #{guest}, happy holidays! What are you doing for the break?"
+      partyguest_response = gets.to_s
+      actions = [
+        "staying at home with my cat.",
+        "making seven gallons of soup.",
+        "figuring out how Rails works."
+      ]
+      puts "Oh, #{partyguest_response}, great! I'm #{actions.sample}"
+      puts "Well, let's hang out soon! Happy New Year!"
+    else
+      puts "Oh, hi #{guest}, is that another bottle of wine and some Brie? Excuse me, won't you..."        
+    end                
+  end
+  bye_actions = [
+    "work my co-op shift.",
+    "repaint my living room.",
+    "do more Ruby tutorials."
+  ]
+  puts "Bye! Have great holidays! I have to go #{bye_actions.sample}"        
+end        
+ 
+conversations(guests, friends)
 
 
